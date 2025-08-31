c0359ad79f312a1cd23b81839b68375361a8426b
Elvis Hasanaj <49841722+ejaz0@users.noreply.github.com>
2025-08-31 06:13:11 +0200

    Get all users

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/ejaz0/blog_aggreator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -10,11 +17,79 @@ func handlerLogin(s *state, cmd command) error {
 	}
 	name := cmd.Args[0]
 
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err == sql.ErrNoRows {
+		return fmt.Errorf("user %q does not exist", name)
+	}
 	if err != nil {
+		return fmt.Errorf("lookup failed: %w", err)
+	}
+	if err := s.cfg.SetUser(name); err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
-	fmt.Println("User switched successfully!")
+	fmt.Println("user switched successfully!")
 	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("not username passed in")
+	}
+	name := cmd.Args[0]
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, name)
+	if err == nil {
+		return fmt.Errorf("user already exists")
+	}
+	if err != nil && err != sql.ErrNoRows {
+		return fmt.Errorf("lookup failed %w", err)
+	}
+	id := uuid.New()
+	now := time.Now()
+	u, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID:        id,
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      name,
+	})
+	if err != nil {
+		return fmt.Errorf("create failed: %w", err)
+	}
+	if err := s.cfg.SetUser(name); err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+	fmt.Printf("created user %q\n", name)
+	log.Printf("user: %+v\n", u)
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	ctx := context.Background()
+	if err := s.db.DeleteUsers(ctx); err != nil {
+		return err
+	}
+	fmt.Println("database reset")
+	return nil
+
+}
+
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %v\n", user.Name)
+	}
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/ejaz0/blog_aggreator/internal/config"
+	"github.com/ejaz0/blog_aggreator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
+
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("Could not open db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
 
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -25,6 +37,9 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+	cmds.register("reset", handlerReset)
+	cmds.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
@@ -37,4 +52,5 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
